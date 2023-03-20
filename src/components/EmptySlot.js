548d2471b29d7233ec24c60cdbdc84f5716722f9
annielam0623 <annzhouyy@gmail.com>
2023-03-19 17:07:00 -0700

    finish

---
+++ src/components/EmptySlot.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import moleHill from '../molehill.png';
+
+
+ const EmptySlot = (props) => {
+    useEffect(() => {
+        let randSeconds = Math.ceil(Math.random() * 5000)
+        let timer = setTimeout(() =>{
+            props.toggle(true)
+        }, randSeconds)
+            return () => clearTimeout(timer)
+    })
+
+    return (
+        <div>
+            <img style={{'width' : '30vw'}} alt=''
+            src={moleHill}
+            onClick={props.handleClick} />
+        </div>
+    )
+
+}
+
+export default EmptySlot
